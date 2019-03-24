80da01387840b7293974b0b71ce4de2e847cac4f
ItaloBruno <italobruno_207@hotmail.com>
2019-03-23 22:59:29 -0300

    Configuraçao para o app rodar no heroku.

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # Importando Bibliotecas
 from flask import Flask
 from flask_restful import Resource, Api
+import os
 
 # Configurações da nossa API
 app = Flask(__name__)
@@ -15,3 +16,7 @@ class Hello(Resource):
 
 # Fazendo associação entre a classe e a rota
 api.add_resource(Hello, "/")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
